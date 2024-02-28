3237a302270ece4f61c570674c80f64231f718d5
risnanto73 <tiolast63@gmail.com>
2024-02-28 10:59:00 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('home.parent')
+
+@section('content')
+    <div class="container">
+        <div class="row card p-4">
+            <h1>
+                Welcome {{ Auth::user()->name }}
+            </h1>
+        </div>
+    </div>
+@endsection
